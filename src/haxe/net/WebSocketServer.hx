86a39d825e5ce8a855fe21e583a4c3c0646f6091
Vyacheslav Moskalev <slouffka@gmail.com>
2020-06-18 07:24:20 +0700

    Change isSecured type to Bool.

--- src/haxe/net/WebSocketServer.hx
+++ src/haxe/net/WebSocketServer.hx
@@ -7,20 +7,19 @@ import sys.ssl.Socket;
 import sys.ssl.Certificate;
 import sys.ssl.Key;
 
-class WebSocketServer { 
-
+class WebSocketServer
+{ 
 	var _isDebug:Bool;
 	var _isSecure:Bool;
 	var _listenSocket:sys.net.Socket;
 	#if neko
 	var keepalive:Dynamic;
 	#end
-	function new(host:String, port:Int, maxConnections:Int, isSecure:Dynamic = null, isDebug:Bool = false) {
+	function new(host:String, port:Int, maxConnections:Int, isSecure:Bool = false, isDebug:Bool = false) {
 		_isDebug = isDebug;
-		_isSecure = isSecure != null;
 		_listenSocket = _isSecure ? new sys.ssl.Socket() : new sys.net.Socket() ;
 		
-		if(_isSecure){
+		if (_isSecure) {
 			cast(_listenSocket, sys.ssl.Socket).setCA( Certificate.loadFile(Reflect.field(isSecure, "CA")) );
         	cast(_listenSocket, sys.ssl.Socket).setCertificate( Certificate.loadFile(Reflect.field(isSecure, "Certificate")), Key.readPEM(sys.io.File.getContent(Reflect.field(isSecure, "Key")), false) );
 			cast(_listenSocket, sys.ssl.Socket).verifyCert = false;
@@ -43,15 +42,14 @@ class WebSocketServer {
 	public function accept():WebSocket {
 		try {
 			var socket:Dynamic = null;
-			 if(_isSecure){
+			if (_isSecure) {
 				socket = cast(_listenSocket, sys.ssl.Socket).accept();
-			}else{
+			} else {
 				socket = _listenSocket.accept();
 			}
 			return WebSocket.createFromAcceptedSocket(Socket2.createFromExistingSocket(socket, _isDebug), '', _isDebug);
 		}
 		catch (e:Dynamic) {
-			
 			return null;
 		}
 	}
